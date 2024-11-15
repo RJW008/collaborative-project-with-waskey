0c0e35a45cd164efba44e336cb6632e53adb4fa2
williamw27 <waskeyw27@stxtigers.com>
2024-11-15 13:52:48 -0500

    Restore '11/15/2024, 1:38:05 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
 let Monthly_Income = game.askForNumber("How much do you make a month?")
 let Affordable_Mortgage = Monthly_Income / 4
 game.splash("You can afford a monthly mortgage of " + "$" + Affordable_Mortgage)
-let Number_of_Workers = game.askForNumber("How many people in your family work?")
-if (Affordable_Mortgage < 500) {
-    game.splash("Try Renting!" + "You are not in a position to buy a house!")
-}
-while (Number_of_Workers > 0) {
-	
-}
