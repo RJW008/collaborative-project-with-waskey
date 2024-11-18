5e52667983b9374b7cb98b9320ea4c4015506113
williamw27 <waskeyw27@stxtigers.com>
2024-11-18 13:18:38 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+pause(5000)
 let Monthly_Income = game.askForNumber("How much do you make a month?")
 let Affordable_Mortgage = Monthly_Income / 4
 game.splash("You can afford a monthly mortgage of " + "$" + Affordable_Mortgage)
